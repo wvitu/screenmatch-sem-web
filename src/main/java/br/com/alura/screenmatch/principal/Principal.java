440975ffbf0adf72d1c41198a0fce9b80dfad6da
Wanderson Vitor Almeida <153443338+wvitu@users.noreply.github.com>
2025-06-08 09:19:33 -0300

    Tratamento de dados na busca por episódios: adicionado filter para o top 5. método para buscar informações de todos episódios adicionado, trazendo nome do episódio, avaliação e data de alançamento

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -7,8 +7,12 @@ import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class Principal {
 
@@ -40,6 +44,17 @@ public class Principal {
 
 
         temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> dadosEpisodios = temporadas.stream()
+                .flatMap(t -> t.episodios().stream())
+                        .collect(Collectors.toList());
+
+        System.out.println("\nTop 5 episódios:");
+        dadosEpisodios.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
 
     }
 }
